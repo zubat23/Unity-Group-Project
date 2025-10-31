ddac14621cc849a5bff7d70d83bca657d47e990c
SCOTT MACKAY <2501389@uad.ac.uk>
2025-10-31 11:40:26 +0000

    Improved enemy
    set a start point that the enemy goes to when not aggroed, and added textures and asset for bear enemy.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,9 +4,11 @@ using UnityEngine.Rendering;
 public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
+    public GameObject inactiveTarget;
     public float speed = 5;
 
     private bool aggressive = false;
+    private bool atStartPoint = false;
     Rigidbody rb;
 
     void Start()
@@ -24,8 +26,21 @@ public class EnemyAI : MonoBehaviour
             rb.transform.position = Vector3.MoveTowards(rb.transform.position, player.transform.position, speed * Time.deltaTime);
             transform.LookAt(player.transform);
         }
+        else if (!atStartPoint)
+        {
+            rb.transform.position = Vector3.MoveTowards(rb.transform.position, inactiveTarget.transform.position, speed * Time.deltaTime);
+            transform.LookAt(inactiveTarget.transform);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        atStartPoint = true;
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        atStartPoint=false;
+    }
     void updateAggression(float distance)
     {
         if (aggressive)
